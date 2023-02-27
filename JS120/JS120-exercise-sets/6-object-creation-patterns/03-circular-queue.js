267f4ece2922b774a16cb140076344063d8ed05f
Coen <code.github@coenvs.nl>
2023-02-27 18:11:28 +0100

    Add exercise 3: Circular Queue

---
+++ JS120/JS120-exercise-sets/6-object-creation-patterns/03-circular-queue.js
@@ -0,0 +1,151 @@
+// A circular queue is a collection of objects stored in a buffer that is
+// treated as though it is connected end-to-end in a circle. When an
+// object is added to this circular queue, it is added to the position
+// that immediately follows the most recently added object, while
+// removing an object always removes the object that has been in the
+// queue the longest.
+
+// This works as long as there are empty spots in the buffer. If the
+// buffer becomes full, adding a new object to the queue requires getting
+// rid of an existing object; with a circular queue, the object that has
+// been in the queue the longest is discarded and replaced by the new
+// object.
+
+// Your task is to write a CircularQueue class that implements a circular
+// queue for arbitrary objects. The class should obtain the buffer size
+// with an argument provided to the constructor, and should provide the
+// following methods:
+
+// - enqueue to add an object to the queue.
+// - dequeue to remove (and return) the oldest object in the queue.
+//   It should return null if the queue is empty.
+
+// You may assume that none of the values stored in the queue are null
+// (however, null may be used to designate empty spots in the buffer).
+
+// Assuming we have a circular queue with room for 3 objects, the
+// circular queue looks and acts like this:
+
+// . . .  all positions empty
+// 1 . .  add 1 to the queue
+// 1 2 .  add 2 to the queue
+// . 2 .  remove oldest item
+// . 2 3  add 3 to the queue
+// 4 2 3  add 4 to the queue, queue is now full
+// 4 . 3  remove oldest item from queue
+// 4 5 3  add 5 to the queue, queue is now full
+// 4 5 6  add 6 to the queue, replaces oldest element
+// 7 5 6  add 7 to the queue, replaces oldest element
+// 7 . 6  remove oldest item from queue
+// . . .  remove oldest item from queue
+// . . .  remove non-existent item from queue (nil)
+
+// SOLUTION
+
+// Methods:
+// enqueue: - add item to the position that immediately follows the most
+//            recently added object.
+//          - if queueu is full, remove the oldest object in the queue.
+// dequeue: - remove (and return) the oldest object in the queue.
+//          - return null if the queue is empty.
+
+class CircularQueue {
+  constructor(length) {
+    this.queue = new Array(length).fill(null);
+    this.newestIdx = 0; // keep track of newest item
+    this.oldestIndexes = [];
+  }
+
+  enqueue(num) {
+    // initialize variable itemPlaced with value of false
+    // iterate through queue, starting at newestIdx, stopping before newestIdx:
+    //   - if value at current idx is null:
+    //       - add num at this idx
+    //       - assign this.newestIdx to idx
+    //       - assign itemPlace to true
+    //       - add idx to this.oldestIndexes
+    //       - quit the loop
+    // if itemPlaced is false:
+    //   - call dequeue
+    //   - call enqueue again
+
+    let itemPlaced = false;
+    let startIdx = this.newestIdx;
+
+    do {
+      if (this.queue[startIdx] === null) {
+        this.queue[startIdx] = num;
+        this.newestIdx = startIdx;
+        itemPlaced = true;
+        this.addToOldest(startIdx);
+        break;
+      }
+
+      if (startIdx === this.queue.length - 1) {
+        startIdx = 0;
+      } else {
+        startIdx += 1;
+      }
+    } while (startIdx !== this.newestIdx);
+
+    if (!itemPlaced) {
+      this.dequeue();
+      this.enqueue(num);
+    }
+  }
+
+  addToOldest(idx) {
+    // add new item index to this.oldestIndexes
+    this.oldestIndexes.push(idx);
+  }
+
+  dequeue() {
+    // - find oldest: first item of this.oldestIndexes
+    // - remove this index number from this.oldestIndexes
+    // - remove the item at this index from this.queue, and
+    //   return this item
+    let oldest = this.oldestIndexes[0];
+    this.oldestIndexes.shift();
+    return this.queue.splice(oldest, 1, null)[0];
+  }
+}
+
+// Examples:
+let queue = new CircularQueue(3);
+console.log(queue.dequeue() === null); // true
+
+queue.enqueue(1);
+queue.enqueue(2);
+console.log(queue.dequeue() === 1); // true
+
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue() === 2); // true
+
+queue.enqueue(5);
+queue.enqueue(6);
+queue.enqueue(7);
+console.log(queue.dequeue() === 5); // true
+console.log(queue.dequeue() === 6); // true
+console.log(queue.dequeue() === 7); // true
+console.log(queue.dequeue() === null); // true
+
+let anotherQueue = new CircularQueue(4);
+console.log(anotherQueue.dequeue() === null); // true
+
+anotherQueue.enqueue(1);
+anotherQueue.enqueue(2);
+console.log(anotherQueue.dequeue() === 1); // true
+
+anotherQueue.enqueue(3);
+anotherQueue.enqueue(4);
+console.log(anotherQueue.dequeue() === 2); // true
+
+anotherQueue.enqueue(5);
+anotherQueue.enqueue(6);
+anotherQueue.enqueue(7);
+console.log(anotherQueue.dequeue() === 4); // true
+console.log(anotherQueue.dequeue() === 5); // true
+console.log(anotherQueue.dequeue() === 6); // true
+console.log(anotherQueue.dequeue() === 7); // true
+console.log(anotherQueue.dequeue() === null); // true
