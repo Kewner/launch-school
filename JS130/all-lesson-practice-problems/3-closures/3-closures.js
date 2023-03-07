6dbf45947cbc561a236f8b1870a8a2ca01de0246
Coen <code.github@coenvs.nl>
2023-03-07 12:17:24 +0100

    Closures practice problem 3

---
+++ JS130/all-lesson-practice-problems/3-closures/3-closures.js
@@ -0,0 +1,28 @@
+// Let's move the variable declaration into makeCounter now. What do the
+// 4 console.log statements at the end of this program print? Try to
+// answer without running the code:
+
+function makeCounter() {
+  let counter = 0;
+
+  return function() {
+    counter += 1;
+    return counter;
+  }
+}
+
+let incrementCounter = makeCounter();
+console.log(incrementCounter()); // 1
+console.log(incrementCounter()); // 2
+
+incrementCounter = makeCounter();
+console.log(incrementCounter()); // 1
+console.log(incrementCounter()); // 2
+
+// The returned function has access to counter, which was declared in the
+// scope of makeCounter. Each time the returned function is called, the
+// value of counter is incremented by 1 and then returned.
+
+// However, on line 18 we reassign incrementCounter to a newly returned
+// counter function. Therefore, a new counter variable is declared inside
+// makeCounter, and given a value of 0.
