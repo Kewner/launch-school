0e90183796044a68d298312ac91b22657860fd45
Coen <code.github@coenvs.nl>
2023-03-08 14:35:56 +0100

    Closures and private data practice problem 3

---
+++ JS130/all-lesson-practice-problems/4-closures-and-private-data/3-private-data.js
@@ -0,0 +1,60 @@
+// Improving the API
+// We resume our discussion of closures and private data by taking another
+// look at the makeList function we wrote in the practice problems.
+
+// Our solution provides a concise but somewhat unclear interface for
+// developers. The function returned by makeList lets the user perform
+// three different actions (adding, removing, and listing) by calling the
+// function with appropriate arguments. It works, but the interface isn't
+// clear. Astonishingly, the single call list('make breakfast') performs
+// two entirely different operations based on the current state of the list!
+
+// We can improve the interface by returning an Object from makeList
+// instead of a function. That lets us create an API that is easy to use
+// and understand.
+
+function makeList() {
+  const todos = [];
+
+  return {
+    list() {
+      if (todos.length === 0) {
+        console.log('This list is empty.');
+      } else {
+        todos.forEach(todo => console.log(todo));
+      }
+    },
+
+    add(todo) {
+      todos.push(todo);
+      console.log(`${todo} added!`);
+    },
+
+    remove(todo) {
+      let index = todos.indexOf(todo);
+      todos.splice(index, 1);
+      console.log(`${todo} removed!`);
+    },
+  }
+}
+
+// Examples
+let list = makeList();
+list.add("peas");
+// peas added!
+
+list.list();
+// peas
+
+list.add("corn");
+// corn added!
+
+list.list();
+// peas
+// corn
+
+list.remove("peas");
+// peas removed!
+
+list.list();
+// corn
