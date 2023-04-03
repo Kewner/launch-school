2388ce4b4610333020e5a1dee196d67f98ad87cf
Coen <code.github@coenvs.nl>
2023-04-03 17:11:28 +0200

    Remove given solutions

--- JS130/04-testing-using-jest/4-todolist/todolist.test.js
+++ JS130/04-testing-using-jest/4-todolist/todolist.test.js
@@ -1,5 +1,3 @@
-// commented solutions are LS solutions
-
 const Todo = require('./todo');
 const TodoList = require('./todolist');
 
@@ -39,10 +37,6 @@ describe('TodoList', () => {
   test('shift() removes and returns the first todo item', () => {
     expect(list.shift()).toEqual(todo1);
     expect(list.size()).toBe(2);
-
-    // let todo = list.shift();
-    // expect(todo).toEqual(todo1);
-    // expect(list.toArray()).toEqual([todo2, todo3]);
   });
 
   test('pop() removes and returns the last todo item', () => {
@@ -53,8 +47,6 @@ describe('TodoList', () => {
 
   test('isDone() checks if all todos are done, returns true or false', () => {
     expect(list.isDone()).toBe(false);
-    // list.markAllDone();
-    // expect(list.isDone()).toBe(true);
   });
 
   test('add() throws error when non todo item is added', () => {
@@ -74,24 +66,12 @@ describe('TodoList', () => {
     list.markDoneAt(0);
     expect(list.allDone().todos).toContain(todo1);
     expect(() => list.markDoneAt(3)).toThrow(ReferenceError);
-
-    // list.markDoneAt(0);
-    // expect(todo1.isDone()).toBe(true);
-    // expect(todo2.isDone()).toBe(false);
-    // expect(todo3.isDone()).toBe(false);
-    // expect(() => list.markDoneAt(3)).toThrow(ReferenceError);
   });
 
   test('markAllDone() marks all todo items as done', () => {
     expect(list.allDone().todos).toEqual([]);
     list.markAllDone();
     expect(list.allDone().todos).toEqual([todo1, todo2, todo3]);
-
-    // list.markAllDone();
-    // expect(todo1.isDone()).toBe(true);
-    // expect(todo2.isDone()).toBe(true);
-    // expect(todo3.isDone()).toBe(true);
-    // expect(list.isDone()).toBe(true);
   });
 
   test('removeAt() removes todo item at given index', () => {
@@ -104,13 +84,6 @@ describe('TodoList', () => {
     let str = `---- ${list.title} ----`;
     [todo1, todo2, todo3].forEach(todo => str += '\n' + todo.toString());
     expect(list.toString()).toBe(str);
-
-//     let string = `---- Today's Todos ----
-// [ ] Buy milk
-// [ ] Clean room
-// [ ] Go to the gym`;
-
-//     expect(list.toString()).toBe(string);
   });
 
   test('toString() returns different string for done todo', () => {
@@ -118,14 +91,6 @@ describe('TodoList', () => {
     let str = `---- ${list.title} ----`;
     [todo1, todo2, todo3].forEach(todo => str += '\n' + todo.toString());
     expect(list.toString()).toBe(str);
-
-//     let string = `---- Today's Todos ----
-// [ ] Buy milk
-// [X] Clean room
-// [ ] Go to the gym`;
-
-//     list.markDoneAt(1);
-//     expect(list.toString()).toBe(string);
   });
 
   test('toString() returns different string for all done todos', () => {
@@ -133,14 +98,6 @@ describe('TodoList', () => {
     let str = `---- ${list.title} ----`;
     [todo1, todo2, todo3].forEach(todo => str += '\n' + todo.toString());
     expect(list.toString()).toBe(str);
-
-//         let string = `---- Today's Todos ----
-// [X] Buy milk
-// [X] Clean room
-// [X] Go to the gym`;
-
-//     list.markAllDone();
-//     expect(list.toString()).toBe(string);
   });
 
   test('forEach() iterates over all todos', () => {
@@ -154,14 +111,5 @@ describe('TodoList', () => {
     const newList = list.filter(todo => todo.isDone());
     const testList = {title: "Today's Todos", todos: [{ title: 'Buy milk', done: true }]};
     expect(newList).toEqual(testList);
-
-    // todo1.markDone();
-    // let newList = new TodoList(list.title);
-    // newList.add(todo1);
-
-    // expect(newList.title).toBe(list.title);
-
-    // let doneItems = list.filter(todo => todo.isDone());
-    // expect(doneItems.toString()).toBe(newList.toString());
   });
 });
