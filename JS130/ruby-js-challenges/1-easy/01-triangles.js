98f5503085f91d849e9ef609c02a649f77530262
Coen <code.github@coenvs.nl>
2023-04-04 19:28:08 +0200

    Triangles exercise, implement Triangle class, first part of kind method, and test suite

---
+++ JS130/ruby-js-challenges/1-easy/01-triangles.js
@@ -0,0 +1,83 @@
+/* Write a program to determine whether a triangle is equilateral,
+isosceles, or scalene.
+
+- An equilateral triangle has all three sides the same length.
+- An isosceles triangle has exactly two sides of the same length.
+- A scalene triangle has all sides of different lengths.
+
+Note: For a shape to be a triangle at all, all sides must be of
+length > 0, and the sum of the lengths of any two sides must be
+greater than the length of the third side.*/
+
+/* PEDAC
+Understanding the problem
+=========================
+Important details:
+- To be a valid triangle, each side length must be greater than 0.
+- To be a valid triangle, the sum of the lengths of any two sides must be
+  greater than the length of the remaining side.
+- An equilateral triangle has three sides of equal length.
+- An isosceles triangle has exactly two sides of the equal length.
+- A scalene triangle has three sides of unequal length (no two sides have
+  equal length).
+
+Examples and test cases
+=======================
+Indicated by the provided test cases:
+- We need a Triangle class with 2 methods:
+  - A constructor method that accepts 3 arguments for 3 side lengths.
+  - A kind method that returns a string representing the type of triangle.
+- Some optional methods might be useful.
+
+Data structures
+===============
+- Side length arguments are numbers.
+- Side lengths will be stored in an array, as the value of a lengths
+  property
+- The return value of the kind method is a string.
+
+Algorithm for constructor
+=========================
+- create length property with an array with the 3 lengths as its value
+- check if each length is greater than 0; if not, throw error
+- check if the sum of the lengths of any two sides is greater than the
+  length of the remaining side; if not, throw error
+
+Algorithm for kind method
+=========================
+- if all side lengths are equal, return 'equilateral'
+- if two exactly two side lengths are equal, return 'isosceles'
+- if all side lengths are different, return 'scalene'
+*/
+
+class Triangle {
+  constructor(sideOne, sideTwo, sideThree) {
+    this.sides = [...arguments];
+    this._validateSides();
+    this._validateTriangle();
+  }
+
+  _validateSides() {
+    if (this.sides.filter(length => length <= 0).length > 0) {
+      throw new Error('All side lengths must be greater than 0.');
+    }
+  }
+
+  _validateTriangle() {
+    const invalidTriangle = new Error('Sum of lengths of any two sides must be greater than length of remaining side.');
+    const [ one, two, three ] = this.sides;
+
+    if (one + two <= three || two + three <= one || one + three <= two) {
+      throw invalidTriangle;
+    }
+  }
+
+  kind() {
+    // - if two exactly two side lengths are equal, return 'isosceles'
+    // - if all side lengths are different, return 'scalene'
+    const sides = this.sides;
+    if (sides.every(side => side === sides[0])) return 'equilateral';
+  }
+}
+
+module.exports = Triangle;
