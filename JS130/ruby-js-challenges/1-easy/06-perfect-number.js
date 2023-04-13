0e4f0657d1aa07789429cc212cb94799cbae4328
Coen <code.github@coenvs.nl>
2023-04-13 19:21:26 +0200

    Write solution code

--- JS130/ruby-js-challenges/1-easy/06-perfect-number.js
+++ JS130/ruby-js-challenges/1-easy/06-perfect-number.js
@@ -25,18 +25,41 @@ Data structures
 - numbers
 - probably an array containing factors
 
-Algorithm for classify method
-=============================
+Algorithm for classify(num) method
+==================================
 - if `num` is negative, throw error
 - find the sum of all divisors using sumOfDivisors
 - if sum is equal to `num`, return 'perfect'
 - else if sum is greater than `num`, return 'abundant'
 - else if sum is less than `num`, return 'deficient'
 
-Algorithm for sumOfDivisors method
-==================================
+Algorithm for sumOfDivisors(num) method
+=======================================
 - initialize `sum` with value of 0
-- start for loop to iterate over numbers from 1 to num (exclusive):
+- start for loop to iterate over numbers from 1 to `num` (exclusive):
   - if current number is a divisor of `num`, add it to `sum`
 - return `sum`
 */
+
+class PerfectNumber {
+  static classify(num) {
+    if (num < 1) throw new Error('Number must be 1 or greater.');
+    const aliquot = this.sumOfDivisors(num);   
+    
+    if (aliquot === num) return 'perfect';
+    if (aliquot > num) return 'abundant';
+    return 'deficient';
+  }
+
+  static sumOfDivisors(num) {
+    let sum = 0;
+
+    for (let current = 1; current < num; current += 1) {
+      if (num % current === 0) sum += current;
+    }
+
+    return sum;
+  }
+}
+
+module.exports = PerfectNumber;
