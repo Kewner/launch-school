b1ff5961afbf17056e5c107ae8af4b382a152a50
Coen <code.github@coenvs.nl>
2023-08-03 15:40:56 +0200

    Add algorithms and code for all methods except isEqual

--- JS130/ruby-js-challenges/2-medium/03-clock/clock.js
+++ JS130/ruby-js-challenges/2-medium/03-clock/clock.js
@@ -10,21 +10,21 @@ Understanding the problem
 Examples/test cases
 ===================
 from the given tests we can conclude that:
-- we need a Clock class with:
-  - a constructor method that accepts 2 arguments representing
-    hours and minutes
-  - a static method at() that:
-    - returns a new Clock object that represents the time according
-      to the given arguments; if no minutes argument is given, 0 will
-      be used as the default value
-  - an instance method add() that:
-    - returns a new Clock object that represents the time of the
-      calling clock, but with the given amount of minutes added to it
-  - an instance method subtract()
-  - an instance method toString() that returns a the time of the clock
-    object as a string in hh:mm format
-  - an instance method isEqual() that tests is 2 Clock objects
-    represent the same time and returns a boolean
+we need a Clock class with:
+- a constructor method that accepts 2 arguments representing
+  hours and minutes
+- a static method at() that:
+  - returns a new Clock object that represents the time according
+    to the given arguments; if no minutes argument is given, 0 will
+    be used as the default value
+- an instance method add() that:
+  - returns a new Clock object that represents the time of the
+    calling clock, but with the given amount of minutes added to it
+- an instance method subtract()
+- an instance method toString() that returns a the time of the clock
+  object as a string in hh:mm format
+- an instance method isEqual() that tests is 2 Clock objects
+  represent the same time and returns a boolean
 
 Data structures
 ===============
@@ -38,13 +38,32 @@ constructor:
 - use value of `hours` argument to define hours property
 - use value of `minutes` argument to defined minutes property
 
-at:
+at (static):
 - calls Clock to return a new Clock object, passing hours and minutes
   arguments (minutes argument's default parameter is 0)
 - returns the new Clock object
 
-add:
+add (instance):
+- reassign `minutes` to `minutes % 1440` (in case minutes >= a day)
+- initialize `hours` to `minutes / 60`, rounded down
+- reassign `minutes` to `minutes % 60`
+- initialize variable `newClock` with return value of calling Clock.at,
+  passing it the hours and minutes of the clock add was called on
+- increment newClock.hours by `hours` and newClock.minutes by `minutes` 
+- if newClock.hours exceeds 23, subtract 24 from it
+- if newClock.minutes exceeds 59, subtract 60 from it and add 1 to
+  newClock.minutes
 
+subtract (instance):
+- reassign `minutes` to `minutes % 1440` (in case minutes >= a day)
+- initialize `hours` to `minutes / 60`, rounded down
+- reassign `minutes` to `minutes % 60`
+- initialize variable `newClock` with return value of calling Clock.at,
+  passing it the hours and minutes of the clock add was called on
+- decrement newClock.hours by `hours` and newClock.minutes by `minutes` 
+- if newClock.minutes is less than 0, reassign it to `60 + newClock.minutes`
+  newClock.minutes and subtract 1 from newClock.hours
+- if newClock.hours is less than 0, reassign it to `12 + newClock.hours`
 */
 
 class Clock {
@@ -60,14 +79,49 @@ class Clock {
   toString() {
     let hours = String(this.hours);
     let minutes = String(this.minutes);
+
     if (hours.length === 1) hours = 0 + hours;
     if (minutes.length === 1) minutes = 0 + minutes;
 
     return `${hours}:${minutes}`;
   }
 
-  add() {
+  add(minutes) {
+    minutes = minutes % 1440;
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
 
+    const newClock = Clock.at(this.hours, this.minutes);
+    newClock.hours += hours;
+    newClock.minutes += minutes;
+
+    if (newClock.hours > 23) newClock.hours -= 24;
+
+    if (newClock.minutes > 59) {
+      newClock.minutes -= 60;
+      newClock.hours += 1;
+    }
+
+    return newClock;
+  }
+
+  subtract(minutes) {
+    minutes = minutes % 1440;
+    let hours = Math.floor(minutes / 60);
+    minutes = minutes % 60;
+
+    const newClock = Clock.at(this.hours, this.minutes);
+    newClock.hours -= hours;
+    newClock.minutes -= minutes;
+
+    if (newClock.minutes < 0) {
+      newClock.minutes += 60;
+      newClock.hours -= 1;
+    }
+
+    if (newClock.hours < 0) newClock.hours += 24;
+
+    return newClock;
   }
 
   isEqual() {
@@ -75,7 +129,13 @@ class Clock {
   }
 }
 
-// let clock = Clock.at(8);
-// console.log(clock.toString());
+let clock0 = Clock.at(23, 30).add(60);
+console.log(clock0.toString());
+
+let clock1 = Clock.at(10).subtract(3061); // subtract 3 hours and 1 minute
+console.log(clock1.toString());
+
+let clock2 = Clock.at(0).subtract(50);
+console.log(clock2.toString());
 
 module.exports = Clock;
