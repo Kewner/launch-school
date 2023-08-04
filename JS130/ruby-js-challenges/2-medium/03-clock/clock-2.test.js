ce8396574997fc873ca6a832c3b4b7729e82470c
Coen <code.github@coenvs.nl>
2023-08-04 09:50:27 +0200

    Add missing tab

--- JS130/ruby-js-challenges/2-medium/03-clock/clock-2.test.js
+++ JS130/ruby-js-challenges/2-medium/03-clock/clock-2.test.js
@@ -21,12 +21,12 @@ describe("Clock", () => {
     expect(newClock).not.toBe(oldClock);
   });
 
-  xtest("subtract fifty minutes", () => {
+  test("subtract fifty minutes", () => {
     let clock = Clock.at(0).subtract(50);
     expect(clock.toString()).toBe('23:10');
   });
 
-  xtest("subtracting does not mutate", () => {
+  test("subtracting does not mutate", () => {
     let oldClock = Clock.at(10);
     let newClock = oldClock.subtract(50);
     expect(newClock).not.toBe(oldClock);
@@ -47,12 +47,12 @@ describe("Clock", () => {
     expect(clock.toString()).toBe('13:01');
   });
 
-  xtest("subtract a few minutes", () => {
+  test("subtract a few minutes", () => {
     let clock = Clock.at(10, 30).subtract(5);
     expect(clock.toString()).toBe('10:25');
   });
 
-  xtest("subtract minutes", () => {
+  test("subtract minutes", () => {
     let clock = Clock.at(10).subtract(90);
     expect(clock.toString()).toBe('08:30');
   });
@@ -64,17 +64,17 @@ describe("Clock", () => {
     expect(clock1.isEqual(clock2)).toBe(true);
   });
 
-  xtest("wrap around at negative midnight", () => {
+  test("wrap around at negative midnight", () => {
     let clock = Clock.at(0, 30).subtract(60);
     expect(clock.toString()).toBe('23:30');
   });
 
-  xtest("subtract more than a day", () => {
+  test("subtract more than a day", () => {
     let clock = Clock.at(10).subtract(3061);
     expect(clock.toString()).toBe('06:59');
   });
 
-xtest("inequivalent clocks", () => {
+  xtest("inequivalent clocks", () => {
     let clock1 = Clock.at(15, 37);
     let clock2 = Clock.at(15, 36);
     let clock3 = Clock.at(14, 37);
@@ -83,7 +83,7 @@ xtest("inequivalent clocks", () => {
     expect(clock1.isEqual(clock3)).toBe(false);
   });
 
-  xtest("wrap around backwards", () => {
+  test("wrap around backwards", () => {
     let clock = Clock.at(0, 30).subtract(60);
     expect(clock.toString()).toEqual('23:30');
   });
